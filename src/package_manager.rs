185baa25f57bf7bc6dcd0c7509de6af76e255252
Mark Toda <mtoda@bitgo.com>
2020-06-22 22:43:01 -0700

    Include rc tags in latest calculation

--- src/package_manager.rs
+++ src/package_manager.rs
@@ -1,5 +1,8 @@
+use std::cmp;
 use anyhow::Result;
+use log::info;
 use serde::Deserialize;
+use semver::Version;
 
 pub trait PackageManager {
     fn get_latest_version_value(package: &str) -> Result<String>;
@@ -7,7 +10,8 @@ pub trait PackageManager {
 
 #[derive(Deserialize)]
 struct Tags {
-    latest: String,
+    latest: Option<String>,
+    rc: Option<String>,
 }
 
 #[derive(Deserialize)]
@@ -26,6 +30,20 @@ impl PackageManager for Npm {
                 .text()?;
 
         let registry_object: NpmRegistryObject = serde_json::from_reader(body.as_bytes())?;
-        Ok(registry_object.tags.latest)
+
+        let latest = registry_object.tags.latest
+            .map(|v| Version::parse(v.as_str()))
+            .unwrap_or(Ok(Version::new(0, 0, 0)))
+            .unwrap_or(Version::new(0, 0, 0));
+
+        let rc = registry_object.tags.rc
+            .map(|v| Version::parse(v.as_str()))
+            .unwrap_or(Ok(Version::new(0, 0, 0)))
+            .unwrap_or(Version::new(0, 0, 0));
+
+        let newest = cmp::max(latest, rc);
+        info!("Found latest version: {} for {}", newest, package);
+
+        Ok(newest.to_string())
     }
 }
